96a81471c07cd89e7769228d3dc1dff6867585c9
Cusanity <wyc935398521@gmail.com>
2021-11-18 14:51:53 -0500

    Combine user servlets into one servlet using reflection

---
+++ src/main/java/cn/cusanity/travel/web/servlet/BaseServlet.java
@@ -0,0 +1,26 @@
+package cn.cusanity.travel.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@WebServlet("/baseServlet")
+public class BaseServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        //Distribute Different Methods
+        String requestURI = req.getRequestURI();
+        String methodName = requestURI.substring(requestURI.lastIndexOf('/') + 1);
+        try {
+            Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(this, req, resp);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/cn/cusanity/travel/web/servlet/UserServlet.java
@@ -0,0 +1,135 @@
+package cn.cusanity.travel.web.servlet;
+
+import cn.cusanity.travel.domain.ResultInfo;
+import cn.cusanity.travel.domain.User;
+import cn.cusanity.travel.service.UserService;
+import cn.cusanity.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/user/*")
+public class UserServlet extends BaseServlet {
+    private final UserServiceImpl service = new UserServiceImpl();
+
+    public void register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //verification code validation
+        String check = request.getParameter("check");
+        //get verification code in session
+        HttpSession session = request.getSession();
+        String checkcode_server = (String) session.getAttribute("CHECKCODE_SERVER");
+        session.removeAttribute("CHECKCODE_SERVER");//one-time verification code
+        if (checkcode_server == null || !checkcode_server.equalsIgnoreCase(check)) {
+            //Wrong Verification Code
+            ResultInfo info = new ResultInfo();
+            info.setFlag(false);
+            info.setErrorMsg("Wrong Verification Code");
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(info);
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().write(json);
+            return;
+        }
+
+        Map<String, String[]> map = request.getParameterMap();
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        boolean flag = service.register(user);
+        ResultInfo info = new ResultInfo();
+        //
+        if (flag) {
+            info.setFlag(true);
+        } else {
+            info.setFlag(false);
+            info.setErrorMsg("Register Failed");
+        }
+
+        //Serialize the 'info' object
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(info);
+
+        //set content-type to application.json
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
+
+    }
+
+    public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String[]> map = request.getParameterMap();
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        User u = service.login(user);
+
+        ResultInfo info = new ResultInfo();
+
+        if (u == null) {
+            //Wrong username/password
+            info.setFlag(false);
+            info.setErrorMsg("Wrong username/password");
+        } else {
+            //Check if the email is verified
+            if ("N".equals(u.getStatus())) {
+                info.setFlag(false);
+                info.setErrorMsg("Your email has not been verified yet.");
+            }
+            //Login successfully
+            else {
+                request.getSession().setAttribute("user", u);
+                info.setFlag(true);
+            }
+        }
+
+        //Response
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(), info);
+    }
+
+    public void findOneUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //Get user info from the session
+        Object user = request.getSession().getAttribute("user");
+
+        //Response
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(), user);
+    }
+
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //Destroy session
+        request.getSession().invalidate();
+
+        //Redirect to login page
+        response.sendRedirect(request.getContextPath() + "/login.html");
+    }
+
+    public void activate(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String code = request.getParameter("code");
+        if (code != null) {
+            boolean flag = service.activate(code);
+            String msg = flag ? "Activation Succeeded, <a href='login.html'>LOGIN</a>" : "Activation FAILED, <a href='register.html'>Register Again</a>";
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().write(msg);
+        }
+    }
+
+}
